de6a9b5a31a1c1705857c8f5d90dd5aee24c757e
PitoEuropeo <alfonmusan14@gmail.com>
2018-01-19 08:46:08 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CuerpoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
